eb8b317882cf66fd659c4b6d11b9ba5dd86d18a6
ahmadfl <ahmadfl@mail.uc.edu>
2018-01-25 12:05:31 -0500

    Linear Regression
    Linear Regression of Bus data and tombstone data

---
+++ 02 tombstonelinear regression R.r
@@ -0,0 +1,34 @@
+a=read.csv("tombstone.csv")
+#parsing into response and covariate variable
+y<-a$Marble.Tombstone.Mean.Surface.Recession.Rate..mm.100years.
+x<-a$Modelled.100.Year.Mean.SO2.Concentration..ug.m..3.
+#plotting the data
+plot(y~x)
+#linear Regression
+model1<-lm(y~x,data=a)
+plot(x,y)
+points(x,model1$fitted.values,pch=20)
+abline(model1)
+#coefficient estimates
+model1$coefficients
+#fitted values
+model1$fitted.values
+#sum of fitted values
+sum(model1$fitted.values)
+#sum of response variables
+sum(y)
+#residual values and sum
+model1$residuals
+sum(model1$residuals)
+#standard errors
+summary(model1)
+max(y)
+which.max(y)
+a[21,1]
+
+#maximum abs residual value
+which.max(abs(model1$residuals))
+a[18,1]
+#mean of covariate and mean of response
+mean(x)
+mean(y)
---
+++ 02linear regressionbus.r
@@ -0,0 +1,35 @@
+a=read.csv("bus.csv")
+#parsing into response and covariate variable
+x<-a$Car.miles.per.year..1000s.
+y<-a$Expenses.per.car.mile..pence.
+#plotting the data
+plot(y~x)
+#linear Regression
+model1<-lm(y~x,data=a)
+plot(x,y)
+points(x,model1$fitted.values,pch=20)
+abline(model1)
+#coefficient estimates
+model1$coefficients
+#fitted values
+model1$fitted.values
+#sum of fitted values
+sum(model1$fitted.values)
+#sum of response variables
+sum(y)
+#residual values and sum
+model1$residuals
+sum(model1$residuals)
+#standard errors
+summary(model1)
+max(y)
+which.max(y)
+a[21,1]
+
+#maximum abs residual value
+which.max(abs(model1$residuals))
+a[18,1]
+#mean of covariate and mean of response
+mean(x)
+mean(y)
+mean(model1$fitted.values)
